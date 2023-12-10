2d4adf1c17b5ea968a1e651841dd27cb9219cb2a
ramonaflores <angaritayelaj@gmail.com>
2023-12-10 14:09:46 -0500

    working spirograph??

--- Python/Damien Hirst Painting with Turtle Graphics/main.py
+++ Python/Damien Hirst Painting with Turtle Graphics/main.py
@@ -1,46 +1,42 @@
-import random
 import turtle
 from random import *
 from turtle import Turtle
+import random
 
 timmy = Turtle()
 timmy.shape("turtle")
 timmy.color("light blue")
-color_list = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', "light blue", "light green", "pink"]
+
 directions = [0, 90, 180, 270]
-timmy.pensize(10)
+timmy.pensize(1)
 timmy.speed("fastest")
+turtle.colormode(255)
+
+
+def spirograph(times):
+    heading = 0
+    for _ in range(times):
+        timmy.color(random_color())
+        timmy.circle(100)
+        timmy.setheading(heading)
+        heading+=10
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
 
 
 def angela_random_walk(times):
     for _ in range(times):
-        timmy.color(color_list[randint(0, len(color_list)-1)])
+        timmy.color(random_color())
         timmy.forward(30)
-        timmy.setheading(directions[randint(0, len(directions)-1)])
+        timmy.setheading(directions[randint(0, len(directions) - 1)])
 
 
-def draw_shape(num_sides):
-    color_n = randint(0, len(color_list) - 1)
-    timmy.color(color_list[color_n])
-    angle = 360 / num_sides
-    for i in range(num_sides):
-        timmy.forward(100)
-        timmy.right(angle)
-
-
-def random_walk():
-    color_n = randint(0, len(color_list) - 1)
-    timmy.color(color_list[color_n])
-    random_x = randint(0, 1)
-    print(random_x)
-    if random_x == 0:
-        timmy.right(90)
-        timmy.forward(50)
-    else:
-        timmy.left(90)
-        timmy.forward(50)
-
-
-angela_random_walk(30)
+spirograph(100)
 screen = turtle.Screen()
 screen.exitonclick()
