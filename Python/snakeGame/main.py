ffbe60d7a36d00789aadd891f770a04b1d836370
ramonaflores <angaritayelaj@gmail.com>
2023-12-22 15:37:20 -0500

    First half done

--- Python/snakeGame/main.py
+++ Python/snakeGame/main.py
@@ -1,26 +1,21 @@
 import time
 from turtle import Screen, Turtle
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 screen.tracer(0)
-segments = []
-for position in starting_positions:
-    new_snake_segment = Turtle(shape="square")
-    new_snake_segment.color("white")
-    new_snake_segment.penup()
-    new_snake_segment.goto(position)
-    segments.append(new_snake_segment)
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.right,"Right")
+screen.onkey(snake.left,"Left")
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for segment in range(len(segments)-1, 0, -1):
-        x = segments[segment - 1].xcor()
-        y = segments[segment - 1].ycor()
-        segments[segment].goto(x, y)
-
+    snake.move()
 screen.exitonclick()
---
+++ Python/snakeGame/snake.py
@@ -0,0 +1,48 @@
+from turtle import Turtle
+
+# Constants in python are written like this
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_snake_segment = Turtle(shape="square")
+            new_snake_segment.color("white")
+            new_snake_segment.penup()
+            new_snake_segment.goto(position)
+            self.segments.append(new_snake_segment)
+
+    def move(self):
+        for segment in range(len(self.segments) - 1, 0, -1):
+            x = self.segments[segment - 1].xcor()
+            y = self.segments[segment - 1].ycor()
+            self.segments[segment].goto(x, y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
